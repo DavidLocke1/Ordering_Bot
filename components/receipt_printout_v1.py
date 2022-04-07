484ea3d97edfb2c5dc86e3069a92de89caf193f3
DavidLocke1 <99290610+DavidLocke1@users.noreply.github.com>
2022-04-07 12:26:06 +1200

    create receipt printout v1
    Started making receipt printing  function.

---
+++ components/receipt_printout_v1.py
@@ -0,0 +1,12 @@
+order_items = []
+order_prices = []
+test = input("1 or 2")
+if test == 1:
+    delivery_type = "pickup"
+else:
+    delivery_type = "delivery"
+
+
+def receipt_printout():
+    if delivery_type == "pickup":
+        for row in table 
