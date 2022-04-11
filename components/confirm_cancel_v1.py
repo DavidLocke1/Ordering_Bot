31834a92180fe750a4670d6b7da444326d7d8ceb
DavidLocke1 <99290610+DavidLocke1@users.noreply.github.com>
2022-04-11 12:35:52 +1200

    Created Confirm_Cancel function
    Made Confirm cancel function work properly and integrated it into the main bot

---
+++ components/confirm_cancel_v1.py
@@ -0,0 +1,17 @@
+print("To confirm your Budgies Burger order type 1")
+print("To cancel your order type 2")
+while True:
+    try:
+        confirmation = int(input("Please type 1 or 2 "))
+        if confirmation == 1:
+            print("Your order has been confirmed")
+            break
+        elif confirmation == 2:
+            print("you have cnceled your order")
+            break
+        else:
+            print("please enter 1 or 2")
+    except ValueError:
+        print("you must enter a number between 1 and 2 for confirm or cancel")
+
+        
---
+++ components/confirm_cancel_v2.py
@@ -0,0 +1,28 @@
+test = int(input("input "))
+if test == 1:
+    delivery_type = "pickup"
+elif test == 2:
+    delivery_type = "delivery"
+
+
+print("To confirm your Budgies Burger order type 1")
+print("To cancel your order type 2")
+while True:
+    try:
+        confirmation = int(input("Please type 1 or 2 "))
+        if confirmation == 1:
+            print("Your order has been confirmed")
+            if delivery_type == "pickup":
+                print("You will recieve a text message when your order is ready to be picked up")
+            elif delivery_type == "delivery":
+                print("Your order will be deliveried shortly")
+            break
+        elif confirmation == 2:
+            print("you have canceled your order")
+            break
+        else:
+            print("please enter 1 or 2")
+    except ValueError:
+        print("you must enter a number between 1 and 2 for confirm or cancel")
+
+        
